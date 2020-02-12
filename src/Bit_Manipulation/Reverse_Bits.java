14c542f9288bb09f8e35d8395b623d56b3c487e4
Ujjwal Pandey <ujwalpande@gmail.com>
2020-02-12 23:43:41 +0530

    Reverse the bits of an 32 bit unsigned integer A & return new number.

---
+++ src/Bit_Manipulation/Reverse_Bits.java
@@ -0,0 +1,62 @@
+/*
+Reverse the bits of an 32 bit unsigned integer A.
+
+
+Problem Constraints
+0 <= A <= 2^32
+
+
+
+Input Format
+First and only argument of input contains an integer A.
+
+
+Output Format
+Return a single unsigned integer denoting minimum xor value.
+
+
+Example Input
+Input 1:
+0
+Input 2:
+3
+
+
+
+Example Output
+Output 1:
+0
+Output 2:
+3221225472
+
+
+ */
+package Bit_Manipulation;
+
+public class Reverse_Bits {
+    public static void main(String[] args) {
+        int N = 3;
+        System.out.println(reverse(N));
+    }
+    public static long reverse(long a) {
+        StringBuilder str = new StringBuilder();
+        long res =0;
+        while (a > 0)
+        {
+            if(a % 2 == 1) {
+                str.append("1");
+            }else{
+                str.append("0");
+            }
+            a = a / 2;
+        }
+        for(int i= str.length(); i<=32; i++){
+            str.append("0");
+        }
+        String[] ch = str.toString().split("");
+        for(int i=31; i>=0; i--){
+            res += Long.parseLong(ch[i])*Math.pow(2,31-i);
+        }
+        return res;
+    }
+}
