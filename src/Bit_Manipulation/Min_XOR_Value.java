4c7bbfba48b20f8bfd855ce3988cf29baaa68ad6
Ujjwal Pandey <ujwalpande@gmail.com>
2020-02-12 19:00:28 +0530

    Finding minimum XOR value of the given Array

---
+++ src/Bit_Manipulation/Min_XOR_Value.java
@@ -0,0 +1,18 @@
+package Bit_Manipulation;
+
+import java.util.Arrays;
+
+public class Min_XOR_Value {
+    public static void main(String[] args) {
+        int[] A = {0, 4, 7, 9};
+        System.out.println(findMinXor(A));
+    }
+    public static int findMinXor(int[] A) {
+        int min = Integer.MAX_VALUE;
+        Arrays.sort(A); // SORTING helped reducing the second loop as we need to find the minimum XOR value from the Array.
+        for(int i=1; i< A.length; i++){
+            min = ((A[i]^A[i-1]) < min)?(A[i]^A[i-1]):min;
+        }
+        return min;
+    }
+}
