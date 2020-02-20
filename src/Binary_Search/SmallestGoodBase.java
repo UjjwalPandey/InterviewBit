567f33e1d517af147fee3c3e03e6edfd9ad1c430
Ujjwal Pandey <ujwalpande@gmail.com>
2020-02-21 00:19:49 +0530

    Optimal Solution for finding Smallest Good Base

--- src/Binary_Search/SmallestGoodBase.java
+++ src/Binary_Search/SmallestGoodBase.java
@@ -1,3 +1,24 @@
+/* Given an integer A, we call k >= 2 a good base of A, if all digits of A base k are 1. Now given a string representing A, you should return the smallest good base of A in string format.
+    Input Format
+        The only argument given is the string representing A.
+    Output Format
+        Return the smallest good base of A in string format.
+    Constraints
+        3 <= A <= 10^18
+    For Example
+        Input 1:
+            A = "13"
+        Output 1:
+            "3"     (13 in base 3 is 111)
+
+        Input 2:
+            A = "4681"
+        Output 2:
+            "8"     (4681 in base 8 is 11111).
+
+
+    Note: Refer http://www.programmersought.com/article/2334663706/
+ */
 package Binary_Search;
 
 import java.math.BigInteger;
@@ -9,7 +30,39 @@ public class SmallestGoodBase {
         for(String s: A) System.out.println(smallestGoodBase(s));
 
     }
+    public static boolean judge(long num, long base){
+        if(base == 1)
+            return false;
+        num -= 1;
+        while(num > 0 && num % base == 0){
+            num /= base;
+            num -= 1;
+        }
+        return num == 0;
+    }
     public static String smallestGoodBase(String A) {
+        long num = Long.valueOf(A);
+        long temp = num - 1;
+        long res = temp;
+        for(long i = 1; i <= Math.pow(num, 1.0 / 3); i++){
+            if((temp) % i != 0)
+                continue;
+            if(judge(num, i))
+                return String.valueOf(i);
+            if(judge(num, (temp) / i))
+                res = (temp) / i;
+        }
+        long root = (long) Math.sqrt(4 * num - 3);
+        if(Math.pow(root, 2) == 4 * num - 3) {
+            long cur = (-1 + root);
+            if(cur != 2 && cur % 2 == 0)
+                res = Math.min(cur / 2, res);
+        }
+        return String.valueOf(res);
+    }
+
+
+    public static String smallestGoodBase_BinarySearch(String A) {
         double val = Double.parseDouble(A);
         if(val < 1 ) {
             BigInteger neg = new BigInteger("18446744073709551615");
