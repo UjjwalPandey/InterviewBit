13e0edfe48645f1edec8d09dc5825dcf2b030e2a
Ujjwal Pandey <ujwalpande@gmail.com>
2020-02-28 17:46:30 +0530

    Question reframed

--- src/Strings/HowManyPalindrome.java
+++ src/Strings/HowManyPalindrome.java
@@ -1,62 +1,64 @@
 package Strings;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class HowManyPalindrome {
     public static void main(String[] args) {
-        String s = "aa";
+        String s = "bxb";
         List<Integer> lower = new ArrayList<>();
-        lower.add(0);
+//        lower.add(1);
+        lower.add(2);
         lower.add(1);
-        lower.add(0);
         List<Integer> upper = new ArrayList<>();
-        upper.add(0);
-        upper.add(1);
+//        upper.add(1);
+        upper.add(3);
         upper.add(1);
         System.out.println(howManyPalindromes(s,lower,upper));
     }
 
     public  static List<Long> howManyPalindromes(String s, List<Integer> lower, List<Integer> upper){
         List<Long> result = new ArrayList<>();
-        int size = Math.min(lower.size(), upper.size());
-        for(int i=0; i<size; i++){
-            String str = s.substring(lower.get(i), upper.get(i)+1);
-            long res = CountPS(str.toCharArray(), str.length());
-            result.add(res);
-            System.out.println("Substr = "+str+"  res = "+res+"  List = "+result);
+        List<String> palindromes = allPalindromeSubstring(s);
+//        System.out.println(palindromes);
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<palindromes.size(); i++){
+            int size = palindromes.get(i).length();
+            int count = hm.getOrDefault(size, 0);
+            hm.put(size,count+1);
+        }
+//        System.out.println(hm);
+
+        int range = Math.min(lower.size(), upper.size());
+        for(int i=0; i< range; i++){
+            long counter =0;
+//            System.out.println(lower+"    "+upper);
+            for(int j= lower.get(i); j <= upper.get(i); j++){
+//                System.out.println(j);
+                counter += hm.getOrDefault(j,0);
+            }
+            result.add(counter);
         }
         return result;
     }
 
-    static long CountPS(char[] str, int n) {
-        if(str.length == 1) return 1;
-        long[][] dp = new long[n][n];
-        boolean[][] P = new boolean[n][n];
+    public static List<String> allPalindromeSubstring(String s) {
+        List<String> list = new ArrayList<String>();
+        for (float pivot = 0; pivot < s.length(); pivot += .5) {
+            float palindromeRadius = pivot - (int)pivot;
 
-        for (int i= 0; i< n; i++)
-            P[i][i] = true;
+            while ((pivot + palindromeRadius) < s.length()
+                    && (pivot - palindromeRadius) >= 0
+                    && s.charAt((int)(pivot - palindromeRadius))
+                    == s.charAt((int)(pivot + palindromeRadius))) {
 
-        for (int i=0; i<n-1; i++){
-            if (str[i] == str[i+1])
-            {
-                P[i][i+1] = true;
-                dp[i][i+1] = 1 ;
+                list.add(s.substring((int)(pivot - palindromeRadius),
+                        (int)(pivot + palindromeRadius + 1)));
+                palindromeRadius++;
             }
         }
-
-        for (int gap=2 ; gap<n; gap++) {
-            for (int i=0; i<n-gap; i++) {
-                int j = gap + i;
-                if (str[i] == str[j] && P[i+1][j-1] )
-                    P[i][j] = true;
-
-                if (P[i][j])
-                    dp[i][j] = dp[i][j-1] + dp[i+1][j] + 1 - dp[i+1][j-1];
-                else
-                    dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1];
-            }
-        }
-        return dp[0][n-1];
+        return list;
     }
 }
