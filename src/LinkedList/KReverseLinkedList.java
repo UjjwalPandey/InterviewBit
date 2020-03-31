e8bcc5fdcfddfcc7caeb65433a19994c921dd313
Ujjwal Pandey <ujwalpande@gmail.com>
2020-03-31 20:49:22 +0530

    Reverse the nodes of the list K at a time and return modified linked list.

--- src/LinkedList/KReverseLinkedList.java
+++ src/LinkedList/KReverseLinkedList.java
@@ -1,8 +1,55 @@
-package LinkedList;// Java program to reverse a linked list in groups of
-// given size
-//
+/*      K reverse linked list
 
-public class ReverseLinkedListGroup {
+    Problem Description
+    Given a singly linked list A and an integer B, reverse the nodes of the list B at a time and return modified linked list.
+
+
+    Problem Constraints
+    1 <= |A| <= 103 K always divides A
+
+
+    Input Format
+    The first argument of input contains a pointer to the head of the linked list. The second arugment of input contains the integer, B.
+
+
+    Output Format
+    Return a pointer to the head of the modified linked list.
+
+
+    Example Input
+    Input 1:
+     A = [1, 2, 3, 4, 5, 6]
+     B = 2
+    Input 2:
+     A = [1, 2, 3, 4, 5, 6]
+     B = 3
+
+
+
+    Example Output
+    Output 1:
+     [2, 1, 4, 3, 6, 5]
+    Output 2:
+     [3, 2, 1, 6, 5, 4]
+
+
+
+    Example Explanation
+    Explanation 1:
+     For the first example, the list can be reversed in groups of 2.
+        [[1, 2], [3, 4], [5, 6]]
+     After reversing the K-linked list
+        [[2, 1], [4, 3], [6, 5]]
+    Explanation 2:
+     For the second example, the list can be reversed in groups of 3.
+        [[1, 2, 3], [4, 5, 6]]
+     After reversing the K-linked list
+        [[3, 2, 1], [6, 5, 4]]
+
+ */
+package LinkedList;
+
+public class KReverseLinkedList {
     Node head;  // head of list
 
     /* Linked list Node*/
@@ -70,7 +117,7 @@ public class ReverseLinkedListGroup {
 
     /* Driver program to test above functions */
     public static void main(String args[]) {
-        ReverseLinkedListGroup llist = new ReverseLinkedListGroup();
+        KReverseLinkedList llist = new KReverseLinkedList();
 
         /* Constructed Linked List is 1->2->3->4->5->6->
            7->8->8->9->null */
