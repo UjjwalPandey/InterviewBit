946f80f10b1d739c554d482bac2830686dee635f
Ujjwal Pandey <ujwalpande@gmail.com>
2020-04-07 17:39:02 +0530

    Merging commits

--- src/LinkedList/RemoveDuplicatesFromSortedList.java
+++ src/LinkedList/RemoveDuplicatesFromSortedList.java
@@ -1,15 +1,66 @@
-/* Remove Duplicates from Sorted List
+/*  Remove Duplicates from Sorted List
 
+    Problem Description
+    Given a sorted linked list, delete all duplicates such that each element appear only once.
 
-Given a sorted linked list, delete all duplicates such that each element appear only once.
+    Problem Constraints
+    0 <= length of linked list <= 1000000
 
-For example,
-Given 1->1->2, return 1->2.
-Given 1->1->2->3->3, return 1->2->3.
-*/
+    Input Format
+    First argument is the head pointer of the linked list.
 
-public class Solution {
-    public ListNode deleteDuplicates(ListNode A) {
+    Output Format
+    Return the head pointer of the linked list after removing all duplicates.
+
+    Example Input
+    Input 1:
+    1->1->2
+    Input 2:
+    1->1->2->3->3
+
+    Example Output
+    Output 1:
+    1->2
+    Output 2:
+    1->2->3
+
+    Example Explanation
+    Explanation 1:
+    each element appear only once in 1->2.
+
+ */
+package LinkedList;
+
+public class RemoveDuplicatesFromSortedList {
+    static class ListNode {
+        public int val;
+        public ListNode next;
+        ListNode(int x) { val = x; next = null; }
+    }
+
+    public static void main(String[] args) {
+        ListNode l;
+        l = new ListNode(1);
+        l.next = new ListNode(1);
+        l.next.next = new ListNode(2);
+        l.next.next.next = new ListNode(3);
+        l.next.next.next.next = new ListNode(3);
+        l.next.next.next.next.next = new ListNode(3);
+        l.next.next.next.next.next.next = new ListNode(4);
+        printList(l);
+        printList(deleteDuplicates(l));
+    }
+
+    private static void printList(ListNode ll) {
+        ListNode temp = ll;
+        while (temp != null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    
+    public static ListNode deleteDuplicates(ListNode A) {
         ListNode temp = A;
         while (temp.next != null){
             ListNode similar = temp.next;
--- src/LinkedList/RemoveDuplicatesFromSortedList_II.java
+++ src/LinkedList/RemoveDuplicatesFromSortedList_II.java
@@ -1,23 +1,62 @@
-public class Solution {
-    public ListNode deleteDuplicates(ListNode A)  {
-        ListNode dummy = new ListNode(0); 
-        dummy.next = A; 
-        ListNode prev = dummy; 
-        ListNode current = A; 
-  
-        while (current != null) { 
-            while (current.next != null && prev.next.val == current.next.val) 
-                current = current.next; 
-  
-            
-            if (prev.next == current){ 
-                prev = prev.next; 
+/*  Remove Duplicates from Sorted List II
+
+    Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+    For example, Given 1->2->3->3->4->4->5, return 1->2->5. Given 1->1->1->2->3, return 2->3.
+
+ */
+package LinkedList;
+
+public class RemoveDuplicatesFromSortedList_II {
+    static class ListNode {
+        public int val;
+        public ListNode next;
+        ListNode(int x) { val = x; next = null; }
+    }
+
+    public static void main(String[] args) {
+        ListNode l;
+        l = new ListNode(1);
+        l.next = new ListNode(1);
+        l.next.next = new ListNode(2);
+        l.next.next.next = new ListNode(3);
+        l.next.next.next.next = new ListNode(3);
+        l.next.next.next.next.next = new ListNode(3);
+        l.next.next.next.next.next.next = new ListNode(4);
+        printList(l);
+        printList(deleteDuplicates(l));
+        l.next.next.next.next.next.next.next = new ListNode(4);
+        printList(l);
+        printList(deleteDuplicates(l));
+    }
+
+    private static void printList(ListNode ll) {
+        ListNode temp = ll;
+        while (temp != null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode deleteDuplicates(ListNode A) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = A;
+        ListNode prev = dummy;
+        ListNode current = A;
+
+        while (current != null) {
+            while (current.next != null && prev.next.val == current.next.val)
+                current = current.next;
+
+
+            if (prev.next == current){
+                prev = prev.next;
             }else{
-                prev.next = current.next; 
+                prev.next = current.next;
             }
-            
-            current = current.next; 
-        } 
+
+            current = current.next;
+        }
         A = dummy.next;
         return A;
     }
