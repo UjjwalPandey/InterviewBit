8bffbc2073d0c21e3e5a7fca6f36da3b47624ab7
Ujjwal Pandey <ujwalpande@gmail.com>
2020-04-10 20:19:25 +0530

    Given an unsorted array, find the maximum difference between the successive elements in its sorted form.

--- src/Array/MaximumConsecutiveGap.java
+++ src/Array/MaximumConsecutiveGap.java
@@ -1,3 +1,17 @@
+/*  Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+    Try to solve it in linear time/space.
+
+    Example :
+
+    Input : [1, 10, 5]
+    Output : 5
+    Return 0 if the array contains less than 2 elements.
+
+    You may assume that all the elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+    You may also assume that the difference will not overflow.
+
+ */
 package Array;
 
 import java.util.Arrays;
@@ -13,7 +27,7 @@ public class MaximumConsecutiveGap {
         System.out.println("Result: "+maximumGap(arr3));
     }
     public static int maximumGap(final int[] A) {
-        if(A.length < 3){
+        if(A.length < 2){
             return 0;
         }
         int len = A.length;
@@ -24,7 +38,7 @@ public class MaximumConsecutiveGap {
         }
         int max = Integer.MIN_VALUE;
         for(int i =0; i<diffArr.length; i++){
-            System.out.println(diffArr[i]);
+//            System.out.println(diffArr[i]);
             if(max < diffArr[i]){
                 max = diffArr[i];
             }
