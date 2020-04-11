47ee74af97ea04b743f1f2be2c242687c7fe0d7f
Ujjwal Pandey <ujwalpande@gmail.com>
2020-04-12 00:32:27 +0530

    Given a doubly linked list of integers with one pointer of each node pointing to the next node (just like in a single link list) while the second pointer, however, can point to any node in the list and not just the previous node.

---
+++ src/LinkedList/CloneALinkedList.java
@@ -0,0 +1,73 @@
+/*  Clone a linked list
+
+    Given a doubly linked list of integers with one pointer of each node pointing
+    to the next node (just like in a single link list) while the second pointer, however, can point to any node
+    in the list and not just the previous node.
+    You have to create a copy of this list and return the head pointer of the duplicated list.
+
+    Input Format
+        The only argument given is head pointer of the doubly linked list.
+    Output Format
+        Return the head pointer of the duplicated list.
+
+    Constraints
+        1 <= length of the list <= 100000
+        1 <= Value of node <= 10^5
+
+    For Example
+    Input 1:
+         1 -> 2 -> 3 -> 4 -> 5
+         random pointer of each node
+         1 -> 5 2 -> 4 3 -> 3 4 -> 2 5 -> 1
+
+
+    Output 1:
+        1 -> 2 -> 3 -> 4 -> 5
+        random pointer of each node
+        1 -> 5 2 -> 4 3 -> 3 4 -> 2 5 -> 1
+
+ */
+package LinkedList;
+
+public class CloneALinkedList {
+    static class ListNode {
+        int val;
+        ListNode next, random;
+        ListNode(int x) {
+            val = x;
+            next = random = null;
+        }
+    }
+    ListNode cloneList(ListNode A) {
+        ListNode ln = null;
+        ListNode head= null;
+        ListNode  parent;
+        ListNode  temp = A;
+        while(temp != null){
+            if(ln == null){
+                ln =  new ListNode(temp.val);
+                head = ln;
+            }else{
+                ln.next =  new ListNode(temp.val);
+                ln = ln.next;
+            }
+            temp = temp.next;
+        }
+
+        // Defining random pointer
+        temp = A;
+        ListNode sr = head;
+        while(sr != null){
+            int num = temp.random.val;
+            ListNode findNode = head;
+            while(findNode.val != num){
+                findNode = findNode.next;
+            }
+            sr.random = findNode;
+            sr = sr.next;
+            temp = temp.next;
+        }
+
+        return head;
+    }
+}
