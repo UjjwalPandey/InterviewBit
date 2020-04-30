787f15bb3469e1755549bf001e8e17205606103d
UjjwalPandey <ujwalpande@gmail.com>
2020-04-30 20:01:31 +0530

    Assign mice to holes so that the time when the last mouse gets inside a hole is minimized.

---
+++ src/GreedyAlgorithms/AssignMiceToHoles.java
@@ -0,0 +1,53 @@
+/*  Assign Mice to Holes
+
+    There are N Mice and N holes are placed in a straight line. Each hole can accomodate only 1 mouse.
+    A mouse can stay at his position, move one step right from x to x + 1, or move one step left from x to x − 1.
+    Any of these moves consumes 1 minute. Assign mice to holes so that the time when the last mouse gets inside a hole is minimized.
+
+    Example:
+
+    positions of mice are:
+    4 -4 2
+    positions of holes are:
+    4 0 5
+
+    Assign mouse at position x=4 to hole at position x=4 : Time taken is 0 minutes
+    Assign mouse at position x=-4 to hole at position x=0 : Time taken is 4 minutes
+    Assign mouse at position x=2 to hole at position x=5 : Time taken is 3 minutes
+    After 4 minutes all of the mice are in the holes.
+
+    Since, there is no combination possible where the last mouse's time is less than 4,
+    answer = 4.
+
+    Input:
+
+    A :  list of positions of mice
+    B :  list of positions of holes
+
+    Output:
+
+    single integer value
+
+
+ */
+package GreedyAlgorithms;
+
+import java.util.Arrays;
+
+public class AssignMiceToHoles {
+    public static void main(String[] args) {
+        int[] mouse = {4, -4, 2};
+        int[] holes = {4, 0, 5};
+        System.out.println(mice(mouse, holes));
+    }
+
+    private static int mice(int[] mouse, int[] holes) {
+        Arrays.sort(mouse);
+        Arrays.sort(holes);
+        int max = Integer.MIN_VALUE;
+        for(int i=0 ; i < mouse.length; i++){
+            max = Math.max(max, Math.abs(mouse[i]-holes[i]));
+        }
+        return max;
+    }
+}
