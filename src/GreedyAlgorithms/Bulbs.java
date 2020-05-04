c59af58287a253bf31458000e893ca5894e71f11
UjjwalPandey <ujwalpande@gmail.com>
2020-05-04 22:24:34 +0530

    Each bulb has a switch associated with it, however due to faulty wiring, a switch also changes the state of all the bulbs to the right of current bulb. Find minimum number of steps to turn on all bulbs

---
+++ src/GreedyAlgorithms/Bulbs.java
@@ -0,0 +1,90 @@
+/*  Bulbs
+
+Problem Description
+
+N light bulbs are connected by a wire.
+
+Each bulb has a switch associated with it, however due to faulty wiring, a switch also changes the state of all the bulbs to the right of current bulb.
+
+Given an initial state of all bulbs, find the minimum number of switches you have to press to turn on all the bulbs.
+
+You can press the same switch multiple times.
+
+Note: 0 represents the bulb is off and 1 represents the bulb is on.
+
+
+
+Problem Constraints
+
+1 <= N <= 5*105
+0 <= A[i] <= 1
+
+
+Input Format
+
+The first and the only argument contains an integer array A, of size N.
+
+
+Output Format
+
+Return an integer representing the minimum number of switches required.
+
+
+Example Input
+
+Input 1:
+
+ A = [0, 1, 0, 1]
+
+Input 2:
+
+ A = [1, 1, 1, 1]
+
+
+
+Example Output
+
+Output 1:
+
+ 4
+
+Output 2:
+
+ 0
+
+
+
+Example Explanation
+
+Explanation 1:
+
+ press switch 0 : [1 0 1 0]
+ press switch 1 : [1 1 0 1]
+ press switch 2 : [1 1 1 0]
+ press switch 3 : [1 1 1 1]
+
+Explanation 2:
+
+ There is no need to turn any switches as all the bulbs are already on.
+
+ */
+package GreedyAlgorithms;
+
+public class Bulbs {
+    public static void main(String[] args) {
+        int[] A= {0, 1, 0, 1};
+        System.out.println(bulbs(A));
+    }
+    public static int bulbs(int[] A) {
+        int count =0;
+        for(int x: A){
+            if(x == 0){
+                count += (count%2 == 0)?1:0;
+            }else {
+                count += (count%2 != 0)?1:0;
+            }
+        }
+
+        return count;
+    }
+}
